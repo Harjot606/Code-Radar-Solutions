2deaa54c097664e66455ad3ec075d365458e4abc
Harjot Singh <honeydhillon606@gmail.com>
2025-04-08 16:49:23 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -11,7 +11,7 @@ int fibonacciSeries(int n){
     fib[0]=0;
     fib[1]=1;
     printf("0 1");
-    for(int i=2;i<n;i++){
+    for(int i=1;i<n;i++){
         fib[i]=fib[i-1]+fib[i-2];
         printf(" %d\t", fib[i]);
     }
