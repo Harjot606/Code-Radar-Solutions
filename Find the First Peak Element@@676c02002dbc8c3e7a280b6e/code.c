db35060e441f7fc214a89731d9a2cb161e549406
Harjot Singh <honeydhillon606@gmail.com>
2025-04-11 11:52:27 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,33 +1,46 @@
-#include<stdio.h>
-int first_peak(int arr[], int N){
-    if(N==1)
+#include <stdio.h>
+
+int first_peak(int arr[], int N) {
+    // Edge case: If there is only one element, return it as the peak
+    if (N == 1)
         return arr[0];
     
-    else if(arr[0]>=arr[1])
+    // If the first element is greater than or equal to the second, return it as the peak
+    else if (arr[0] >= arr[1])
         return arr[0];
     
-    for(int j=1;j<N-1;j++){
-        if(arr[j]>arr[j-1] && arr[j]>arr[j+1]){
+    // Loop through the middle elements to check for peak
+    for (int j = 1; j < N - 1; j++) {
+        if (arr[j] > arr[j - 1] && arr[j] > arr[j + 1]) {
             return arr[j];
         }
     }
-    if(arr[N-1]>arr[N-2]){
-        return arr[N-1];
-        }
+
+    // If the last element is greater than or equal to the second last, return it as the peak
+    if (arr[N - 1] > arr[N - 2]) {
+        return arr[N - 1];
+    }
+
+    // If no peak found, return -1 (this should not happen for non-decreasing arrays)
     return -1;
 }
-int main(){
+
+int main() {
     int N;
     scanf("%d", &N);
-     int arr[N];
-     for(int i=0;i<N;i++){
+
+    // If N is less than 1, it's an invalid size, so return early
+    if (N <= 0) {
+        return 0;
+    }
+
+    int arr[N];
+    for (int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
-     }
+    }
 
     int peak = first_peak(arr, N);
     printf("%d", peak);
-    else{
-        printf("-1");
-    }
+ 
     return 0;
 }
